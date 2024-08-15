aae803d06f96dfca71f6e807cbfef50ddccf9cee
sodamid <sodamid@DESKTOP-1RRSPKK>
2024-08-16 01:17:25 +0330

    Add project files.

---
+++ FibonacciNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //fibonacci = fib(n-1) + fib(n-2)
+            int a = 0, b = 1, c;
+            Console.WriteLine("Enter a number please:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nResult:");
+            Console.WriteLine("\n" + a + "\n\n" + b + "\n");
+            for(int i = 2; i<=n; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c + "\n");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
